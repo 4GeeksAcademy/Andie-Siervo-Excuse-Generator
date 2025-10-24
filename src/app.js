fa401811855d80071243ab0b11dff40d424cafad
andreasiervo <andrea.c.siervo@gmail.com>
2025-10-24 20:52:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi loro', 'Mi vecino', 'Un extraterrestre'];
+let action = ['se comió', 'mojó', 'rompió', 'perdió', 'robó', 'aplastó'];
+let what = ['mi tarea', 'mi teléfono', 'las llaves del coche', 'mi cuaderno', 'el informe', 'mi zapato'];
+let when = ['antes de clase', 'mientras dormía', 'mientras cocinaba', 'cuando iba saliendo', 'durante la lluvia', 'justo ahora'];
 
+function generarExcusa() {
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let cuando = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${quien} ${accion} ${que} ${cuando}.`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = document.getElementById("excuse");
+  excusa.innerHTML = generarExcusa();
 };
